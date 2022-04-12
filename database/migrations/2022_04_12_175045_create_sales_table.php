e5696750cd93494015244831aecfcb254deea520
Olly <developer.olly@gmail.com>
2022-04-12 19:22:08 +0100

    feat: sales migrations

--- database/migrations/2022_04_12_175045_create_sales_table.php
+++ database/migrations/2022_04_12_175045_create_sales_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('product_id');
+            $table->integer('quantity');
+            $table->enum('status', ['cart', 'pending', 'rejected']);
             $table->timestamps();
         });
     }
